16fde68032d1e46f6b4affe3f1004e8a04d7bff2
greenina <igreen0485@gmail.com>
2022-12-03 23:30:35 +0900

    Enable context capturing

--- src/ui.ts
+++ src/ui.ts
@@ -15,20 +15,24 @@ export async function showQuickPick(text: CaptureSource) {
 	const dom = parser.parseFromString(imgTag);
 	const element = dom.getElementsByTagName('img');
 	const div_elem = dom.getElementsByTagName('div');
+	let div_text = "Not Captured";
 	if(!element || !div_elem){
 		return window.showInformationMessage('Check if you selected the valid code');
 	}
 	const srcText = element[0].getAttribute("src");
-	// const div_text = div_elem[0].innerHTML;
+	if(div_elem){
+		div_text = div_elem[0].innerHTML;
+	}
+	
 	if(!srcText){
 		return window.showInformationMessage('Check if you entered valid src');
 	}
-	const altRec = getCaptionRec(srcText);
-	reLoadHistory(srcText, altRec, text);
+	const altRec = getCaptionRec(srcText, div_text);
+	reLoadHistory(srcText, altRec, text, div_text);
 
 }
 
-async function reLoadHistory(src: string, elem:string, original: CaptureSource){
+async function reLoadHistory(src: string, elem:string, original: CaptureSource, div_text: string){
 	// console.log("reLoadHistory of ", src, elem);
 	const list = ["Write from Scratch","Draw Another Recommendation"];
 	list.push(elem);
@@ -42,33 +46,34 @@ async function reLoadHistory(src: string, elem:string, original: CaptureSource){
 		}
 	});
 	if(index == 1){
-		const newRec = getCaptionRec(src);
+		const newRec = getCaptionRec(src, div_text);
 		pastRec.push(elem);
 		// console.log("Recorrd Updated: ", pastRec);
-		reLoadHistory(src, newRec, original);
+		reLoadHistory(src, newRec, original, div_text);
 	} else {
-		showInputBox(src, result, original);
+		showInputBox(src, result, original, div_text);
 	}
 }
 
-function getCaptionRec(src:string) {
+function getCaptionRec(src:string, div_text: string) {
 	if(!src){
 		console.log("No src to get cap");
 		return "Dummy Rec";
 	}
-	// axios
-	// 	.post(`URL`, {
-	// 		imgUrl: src
-	// 		// 인준이 모델 적용 시 추가 예정 : context: string 
-	// 	})
-	// 	.then((res) => {
-	// 		return res; // res type: json : {alt: String}
-	// });
+	axios
+		.post(`URL`, {
+			imgUrl: src,
+			context: div_text
+			// 인준이 모델 적용 시 추가 예정 : context: string 
+		})
+		.then((res) => {
+			return res; // res type: json : {alt: String}
+	});
 	const i = Math.round(Math.random()*3);
 	return tmpRecList[i];
 }
 
-export async function resetAlt(document: TextDocument, original: CaptureSource, result: string|undefined, src:string){
+export async function resetAlt(document: TextDocument, original: CaptureSource, result: string|undefined, src:string, div_text: string){
 	const editor = window.activeTextEditor;
 	if(!original.lineRange){
 		return{};
@@ -78,14 +83,15 @@ export async function resetAlt(document: TextDocument, original: CaptureSource, 
 		return{};
 	}
 	// TODO: 아래처럼 하지 말고 original parsing 해서 height, weight 같은 다른 attribute도 반영 시키기
-	const final = `<img src="` + src + `" alt="` + result + `" />`;
+	const text_div = `<div>` + `${div_text == "Not Captured"?null:div_text}`; 
+	const final = text_div + `</div>\n<img src="` + src + `" alt="` + result + `" />`;
 	editor?.edit(editBuilder => {
 		editBuilder.replace(selection, final);
 	});
 	
 	pastRec = [];
 }
-export async function showInputBox(src: string, selected: string|undefined, original: CaptureSource) {
+export async function showInputBox(src: string, selected: string|undefined, original: CaptureSource, div_text: string) {
 	const result = await window.showInputBox({
 		value: selected,
 		placeHolder: 'You can modify your selection',
@@ -102,7 +108,7 @@ export async function showInputBox(src: string, selected: string|undefined, orig
 		finalAlt: result
 	});
 	// respond는 success 여부 boolean으로 보내주면 될듯! 
-	resetAlt(window.activeTextEditor.document, original, result, src);
+	resetAlt(window.activeTextEditor.document, original, result, src, div_text);
 	// window.showInformationMessage(`Got: ${result}`);
 }
 

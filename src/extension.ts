ebcd383138520b33a07e7a9179ad0d57d4646340
greenina <igreen0485@gmail.com>
2022-12-10 18:52:44 +0900

    Modify showInfoMessage slightly

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,5 @@
 import * as vscode from 'vscode';
 import capture, { shouldResumeCapture } from './capture/command';
-import * as ui from './ui';
-
-
-const COMMAND = 'code-actions-sample.command';
 
 export function activate(context: vscode.ExtensionContext) {
 	const captureCommand = vscode.commands.registerCommand('altelper.capture', capture);
--- src/ui.ts
+++ src/ui.ts
@@ -4,7 +4,6 @@ import DomParser = require('dom-parser');
 import axios from 'axios';
 
 export let pastRec:string[] = [];
-export const tmpRecList = ["1Rec",'2Rec', '3Rec', '4Rec'];
 export async function showQuickPick(text: CaptureSource) {
 	// TODO: axios로 capturesource에 의한 caption recommentation 받아오기
 	const imgTag = text.content;
@@ -30,15 +29,18 @@ export async function showQuickPick(text: CaptureSource) {
 	if(!srcText){
 		return window.showInformationMessage('Check if you entered valid src');
 	}
-	const altRec = getCaptionRec(srcText, div_text);
-	reLoadHistory(srcText, altRec, text, div_text);
+	const altRec = await getCaptionRec(srcText, div_text);
+	await reLoadHistory(srcText, altRec, text, div_text);
 
 }
 
-async function reLoadHistory(src: string, elem:string, original: CaptureSource, div_text: string){
+async function reLoadHistory(src: string, elem:string|undefined, original: CaptureSource, div_text: string){
 	// console.log("reLoadHistory of ", src, elem);
-	const list = ["Write from Scratch","Draw Another Recommendation"];
-	list.push(elem);
+	if(!elem){
+		return {};
+	}
+	const list = ["Write from scratch",elem];
+	// list.push(elem);
 	const uiList = list.concat(pastRec);
 	let index = 0;
 	const result = await window.showQuickPick(uiList, {
@@ -48,34 +50,40 @@ async function reLoadHistory(src: string, elem:string, original: CaptureSource, 
 			// return window.showInformationMessage('Selected!');
 		}
 	});
-	if(index == 1){
-		const newRec = getCaptionRec(src, div_text);
-		pastRec.push(elem);
-		// console.log("Recorrd Updated: ", pastRec);
-		reLoadHistory(src, newRec, original, div_text);
+	// if(index == 1){
+	// 	const newRec = await getCaptionRec(src, div_text);
+	// 	pastRec.push(elem);
+	// 	// console.log("Recorrd Updated: ", pastRec);
+	// 	reLoadHistory(src, newRec, original, div_text);
+	// } else {
+	// 	showInputBox(src, result, original, div_text);
+	// }
+	if(index == 0){
+		showInputBox(src, "", original, div_text);
 	} else {
 		showInputBox(src, result, original, div_text);
 	}
 }
 
-function getCaptionRec(src:string, div_text: string) {
+async function getCaptionRec(src:string|undefined, div_text: string) {
 	if(!src){
 		console.log("No src to get cap");
-		return "Dummy Rec";
+		return;
 	}
+
 	// TOJIHO: 그냥 captioning model에서는 div_text가 없을테고 이때 default 값은 "Not Captured"로 설정돼있어. 
 	// "Not Captured"일 경우 인준이 모델 말고, huggingface 모델에 imgUrl input만 받아서 alt text res 주면 될 것 같아.  
 
-	// axios
-	// 	.post(`URL`, {
-	// 		imgUrl: src,
-	// 		context: div_text
-	// 	})
-	// 	.then((res) => {
-	// 		return res; // res type: json : {alt: String}
-	// });
-	const i = Math.round(Math.random()*3);
-	return tmpRecList[i];
+	const result = await axios
+		.post(`http://aria.sparcs.org:15001/api/v1/predict`, {
+			url: src,
+			// context: div_text
+		})
+		.then((res) => {
+			console.log("RES: ", res.data.alt);
+			return res.data.alt; // res type: json : {alt: String}
+	});
+	return result;
 }
 
 export async function resetAlt(document: TextDocument, original: CaptureSource, result: string|undefined, src:string, div_text: string){
@@ -121,7 +129,7 @@ export async function showInputBox(src: string, selected: string|undefined, orig
 
 export async function showImgCaptionMade(source: CaptureSource) {
   await window.showInformationMessage(
-    'Image Caption Created'
+    'Image Caption Recommendation Made\n If yet, please wait for a few sec'
   );
 }
 
